eb34e46083dfaef250906c655f1f2e3f7250a69e
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2022-06-09 22:15:18 +0000

    WIP: new aruco detector

---
+++ scripts/aruco_detector.py
@@ -0,0 +1,363 @@
+#!/usr/bin/env python3
+"""
+author:     FRANCESCO CERRI
+            francesco.cerri2@studio.unibo.it
+            github.com/gnoccoalpesto
+
+for:        ALMA-X ROVER TEAM
+            github.com/alma-x
+
+            EUROPEAN ROVER CHALLENGE
+
+BIG THANKS TO FEDERICO ZECCHI WHO HELPED JUMPSTART THIS CODE'S FIRST VERSION
+
+
+# WHAT THIS CODE DOES ########################
+- ACQUIRES IMAGE FROM A CAMERA (IN THIS CASE, GRIPPER'S CAMERA)
+- SELECTED A DICTIONARY, DETECTS ALL ARUCO MARKERS IN THE IMAGE
+- FOR EACH MARKER, PUBLISHES POSITION AND ORIENTATION WRT CAMERA
+
+
+# CODE'S STRUCTURE
+
+class
+    init
+    subscribers
+    detector function
+    geometric function
+    cosmethic function
+"""
+import rospy
+import numpy as np
+import sys
+import os
+from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import Float32MultiArray
+#from sensor_msgs.msg import PointCloud2 as sensPCld
+
+from arm_control.srv import aruco_service,aruco_serviceResponse
+from arm_control.srv import cv_server,cv_serverResponse, cv_serverRequest
+from arm_control.msg import cv_to_bridge as bridge_msg
+
+import cv2 
+from cv2 import aruco as aruco
+from cv_bridge import CvBridge, CvBridgeError
+from roscamLibrary3 import nsingleAruRelPos as singleAruRelPos
+
+#PUBLISHER DI ARRAY:
+#aruco_position_pub = rospy.Publisher('/almax/aruco_target',Float64MultiArray,queue_size=20)
+#array = [69.1,0,1,33,1,1,1,0]
+#robaccia = Float64MultiArray(data=array)
+#aruco_position_pub.publish(robaccia)
+#------------------------------------------------
+
+class MarkerDetector:
+    def __init__(self) -> None:
+
+        self.MAX_VISIBLE_MARKERS=14        
+        self.bridge_topic='aruco_bridge_opencv'
+        self.markers_topic='?'
+        self.bridge_pub = rospy.Publisher(self.bridge_topic, bridge_msg, queue_size=1)
+        self.markers_pub=rospy.Publisher(self.markers_topic,Float32MultiArray,queue_size=self.MAX_VISIBLE_MARKERS)
+        self.do_exit=False
+
+        self.ARUCO_PARAMS = aruco.DetectorParameters_create()
+
+
+        self.supported_aruco_dicts={ 'original':aruco.DICT_ARUCO_ORIGINAL
+                                    ,'51000':aruco.DICT_5X5_1000
+                                    ,'61000':aruco.DICT_6X6_1000
+                                    ,'71000':aruco.DICT_7X7_1000
+                                    }
+
+        self.SELECTED_DICT='original'
+        self.loadArucoDict(self.SELECTED_DICT)
+
+        self.CAMERA_MATR=np.ndarray
+        self.CAMERA_DISTORSION_COEFS=np.ndarray
+        self.CAMERA_FOCAL_LEN=np.ndarray
+
+
+
+    def loadArucoDict(self,dict_name):
+        """
+        load data relatively to selected dictionary
+        :param dict_name: string identifying name of dictionary;
+                            as in self.supported_aruco_dicts
+        """
+        requested_dict=self.supported_aruco_dicts[dict_name]
+        self.ARUCO_DICT=aruco.Dictionary_get(requested_dict)
+    
+
+    def loadCameraParams(self,camera_name):
+        """
+        load intrinsecal parameters of the camera
+        params will be retrieved from camera_name/camera_info topic
+        NOTE: no callback since it is supposed that params won't change in 
+
+        :param camera_name: string identifying camera name;
+                            must appear in the published ones
+        """
+        print('loading intrinsecal params from camera: {}'.format(camera_name))
+        info_topic=camera_name+"/camera_info"
+        info_msg=rospy.wait_for_message(info_topic,CameraInfo)
+
+        self.CAMERA_MATR=np.reshape(info_msg.K,[3,3])
+        self.CAMERA_DISTORSION_COEFS=info_msg.D
+        self.CAMERA_FOCAL_LEN=np.mean([np.ravel(self.CAMERA_MATR[0])[0],np.ravel(self.CAMERA_MATR[1])[1]])
+    
+#------------------------------------------------------------
+panel_aruco_dict={  'switch1':{'id':1,'size':50},
+                    'switch2':{'id':2,'size':50},
+                    'switch3':{'id':3,'size':50},
+                    'switch4':{'id':4,'size':50},
+                    'switch5':{'id':5,'size':50},
+                    'switch6':{'id':6,'size':50},
+                    'switch7':{'id':7,'size':50},
+                    'switch8':{'id':8,'size':50},
+                    'switch9':{'id':9,'size':50},
+                }
+
+#TODO: MAKE THIS INTO A DICTIONARY AS ABOVE
+# targetList=[[1,50],
+#             [2,50],
+#             [3,50],
+#             [4,50],
+#             [5,50],
+#             [6,50],
+#             [7,50],
+#             [8,50],
+#             [9,50],
+#             [10,40],
+#             [11,50],
+#             [12,50],
+#             [13,40],
+#             [14,50]]
+targetList=[[1,50],
+            [2,50],
+            [3,50],
+            [4,50],
+            [5,50],
+            [6,50],
+            [7,50],
+            [8,50],
+            [9,50],
+            [10,40],
+            [11,50],
+            [12,50],
+            [13,40],
+            [14,50],
+            [102,40],
+            [104,40],
+            [106,40],
+            [108,40]]
+aruco_success=False
+
+#TODO: FOLLOWING TARGET STRING DOES NOT WORK
+#targetList=['panelSwitch8','panelSwitch7','panelSwitch6','panelSwitch5','panelSwitch4'
+#            ,'panelSwitch3','panelSwitch2','panelSwitch1']
+
+#TODO? WHY IS A TARGET STRING NEEDED?
+targetList=[[102,40],[104,40],[106,40],[108,40]]
+targetCounter=0
+targetListLen=len(targetList)
+remaining_targets=targetListLen
+
+#global findNewTarget
+#findNewTarget=1
+
+#def receiveTargetRequest():
+#    selectedTarget=read_somewhere(targetRequestTopic)
+#    (targetMarkId,targetMarkSize)=loadTargetData(selectedTarget)
+
+#def loadTargetData(requestString):
+#    return aruTargetDict[requestString]
+
+#TODO? IS global NEEDED HERE?
+global targetMarkId,targetMarkSize
+
+#----------------------------------------
+bridge=CvBridge()
+ 
+def callbackRaw(raw_img):
+    global aruco_success
+    global msgVector
+    global msgRotMatrix
+    global targetCounter
+    global findNewTarget
+    global remaining_targets
+    global bool_exit
+
+    if bool_exit:
+        cv2.destroyAllWindows()
+        os._exit(os.EX_OK)
+
+    # TODO: ADD try:
+    cv_image=bridge.imgmsg_to_cv2(raw_img, desired_encoding='passthrough')
+    cv_gray=cv2.cvtColor(cv_image,cv2.COLOR_RGB2GRAY)
+    # except CvBridgeError: ...
+
+    msg=bridge_msg()
+    # msg.aruco_found=[False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+    msg.aruco_found=[False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+    
+    #TODO: WHY targetCounter goes above len(targetList) even if forced otherwise?
+    (targetMarkId,targetMarkSize)=tuple(targetList[targetCounter])
+    detCorners, detIds, _ = aruco.detectMarkers(cv_gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+        
+    if detIds is not None and len(detIds) >= 1: # Check if at least one marker has been found
+        
+        detAruImg = aruco.drawDetectedMarkers(cv_image.copy(), detCorners, borderColor=(0, 255, 0))
+        
+        aruco_success=False 
+
+        for mId, aruPoints in zip(detIds, detCorners):
+            if len(msg.aruco_found)>int(mId):
+                msg.aruco_found[int(mId)]=True
+            if mId==targetList[targetCounter][0]:    
+                detAruImg,aruDistnc,Pmatr=singleAruRelPos(detAruImg,aruPoints,mId,targetMarkSize,
+                                            cameraMatr,cameraDistCoefs,tglDrawMark=1)
+                
+                rotMatr,tVect=Pmatr[0:3,0:3],Pmatr[0:3,3]
+                msgRotMatrix=rotMatr
+                msgVector=tVect
+                
+                aruco_success=True
+                # remaining_targets=targetListLen-targetCounter-1
+                #if targetCounter<targetListLen-1:
+                #    targetCounter+=1
+                
+    else:
+        aruco_success=False
+        detAruImg=cv_image.copy()
+
+    #    newSize,_=int(np.shape(detAruImg))
+    #    detAruImg=cv2.resize(detAruImg,newSize)
+    cv2.imshow('detected markers',detAruImg)
+
+    msg.success=aruco_success
+
+    msg.id_aruco=targetCounter+1
+
+    if msg.success:
+        #msg.x=0.001*msgVector[2] +(recovLenRatio*0.08 if tglWristLengthRecovery else 0)
+        msg.x=0.001*msgVector[0]
+        msg.y=0.001*msgVector[1]
+        msg.z=0.001*msgVector[2]
+        msg.vector=msgRotMatrix.flatten()
+        #print(msg.vector)
+    
+    pub.publish(msg)
+    key = cv2.waitKey(12) & 0xFF
+    if key == ord('q'):
+      rospy.on_shutdown()
+        
+    
+#-----------------------------------------------------------------
+
+msgVector=[0,0,0]#np.zeros([1,3])
+msgRotMatrix=[[0,0,0,],[0,0,0],[0,0,0]]#np.zeros([3,3])
+
+        
+tglWristLengthRecovery=1
+# recovered percentage
+recovLenRatio=1
+
+def callback_service(req):
+    global aruco_success,msgVector,msgRotMatrix,targetCounter,findNewTarget,remaining_targets,bool_exit
+    
+    print('Service received')
+    if req.message=="exit":
+        bool_exit=True
+    #if req.next_aruco:
+    if req.message=="select_next_aruco":
+        #if targetCounter<targetListLen-1:
+        #    targetCounter=targetCounter+1
+        #remaining_targets=targetListLen-targetCounter-1
+        targetCounter=int(req.second_information)-1
+
+    #print('Arucopy:\nService received')
+    #print('Service received')
+    #print('Target number:'+str(targetCounter))
+    #print('Remaining_targets:'+str(remaining_targets))
+    #print('TargetListLen:'+str(targetListLen))
+    return cv_serverResponse(
+        success=aruco_success,
+        moreTargets=remaining_targets,
+        x=0.001*msgVector[2] +(recovLenRatio*0.08 if tglWristLengthRecovery else 0),#[m]
+        y=0.001*msgVector[0],   
+        z=0.001*msgVector[1],
+        vector=np.ravel(msgRotMatrix)#flattened array
+        )
+
+
+#NOTE:
+#   tVect       tool0/maniulator e.e reference frame
+#    X              Z
+#    Y              x
+#    Z              Y
+#rotation matrix: tVect=R*tool0_vects
+#
+#        	R= 0 0 1
+#           1 0 0
+#           0 1 0
+#------------------------------------------------------
+
+#TODO: TO MAKE THIS LISTENER LESS GENERIC (:ONLY Image OBJ TO WATCH...)
+#      PUT THIS INTO A CLASS
+#       ALSO, GENERIC PARAM PASSING COULD BE AVOIDED
+#TODO: REMOVE GENERIC CALLBACK PASSING
+
+def listener(myCam,myTop,myType,myCallk):
+    node_name='camera_listener'
+    rospy.init_node(node_name, anonymous=False)
+    loadCameraParam(myCam)
+    print('node: {}'.format(node_name))
+    print('camera: {}'.format(myCam))
+    print('input topic: {}'.format(myTop))
+
+    rospy.Subscriber(myCam+myTop,myType,myCallk,queue_size = 1)
+    rospy.Publisher('aruco_bridge_opencv', bridge_msg, queue_size=10)
+    rospy.Service('cv_server', cv_server, callback_service)
+    
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:#
+        print('Closing node: {}'.format(node_name))
+    cv2.destroyAllWindows()
+    
+#---------------------------------------------------------------
+    
+#TODO? DOES SPLITTING CAMERA AND "TOPIC" NAME HAVE ANY, NON COSMETHIC, MEANING?    
+camDict={'moving':"/camera_image",
+            'fixed':"/camera_image_fix"}
+
+topicDict={'raw':("/color/image_raw",
+                   Image,
+                    callbackRaw)    
+            }   
+
+################===========================#####################
+if __name__ == '__main__':
+    #TODO: FIX CAMERA SELECTION
+    # myCamera=camDict['moving']
+    # myTopicFull=topicDict['raw']
+    myCamera="/camera_image"
+    myTopicFull=("/image_raw",Image,callbackRaw)
+    
+    #TODO: MOVE WAITING INSIDE THE CLASS
+    print('Detector Node for Panal Aruco Visual Markers')
+    print('connecting to:'+myCamera+myTopicFull[0]+'...')
+    listener(myCamera,myTopicFull[0],myTopicFull[1],myTopicFull[2])
+
+"""
+bibliography
+    (1)
+compressed images
+cast in np array and cv2.imdecode
+http://wiki.ros.org/rospy_tutorials/Tutorials/WritingImagePublisherSubscriber
+   (2)
+https://answers.ros.org/question/249775/display-compresseddepth-image-python-cv2/
+  (3)
+https://strawlab.github.io/python-pcl/
+"""
--- scripts/lib_aruco.py
+++ scripts/lib_aruco.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import cv2
 from cv2 import aruco as aruco
-import rospy
+#import rospy
 #from std_msgs.msg import String
 #from PIL import Image
 #import matplotlib.pyplot as plt
 import numpy as np
-from sensor_msgs.msg import Image 
+#from sensor_msgs.msg import Image asImage
 #from sensor_msgs.msg import CompressedImage
 #frofm sensor_msgs.msg import PointCloud2 as sensPCld
 #from std_msgs.msg import Float64MultiArray
@@ -283,140 +283,9 @@ def nsingleAruRelPos(queryImg,corners,Id,markerSize_mm,camera_matrix,camera_dist
     return queryImg,distnc_mm,P
 
 """
-#!/usr/bin/env python
-import cv2 as cv
-from cv2 import aruco as aruco
-import numpy as np
-
-
 
 #################### COSMETHIC FUNCTIONS ############################
 
-def IdOverAruco(ids, corners, grayQueryImg):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    arucoIDImg = np.copy(grayQueryImg)
-    # Check if some aruco has been found
-    if ids is not None and len(ids) >= 1:
-        for i, corner in zip(ids, corners):
-            #print("Corners:", corner)
-            cv2.fillPoly(arucoIDImg, corner.astype(int), (230, 230, 230))
-
-            (textX, textY )= np.abs(corner[0][0] + corner[0][2]) / 2
-            textsizeX, textsizeY = cv2.getTextSize(str(i[0]), font, 1, 3)[0]
-            textX = (textX - textsizeX / 2).astype(int)
-            textY = (textY + textsizeY / 2).astype(int)
-            cv2.putText(arucoIDImg, str(i[0]), (textX, textY), font, 1, (0, 0, 0), 2)
-
-    return arucoIDImg
-
-def distOverAruco(distanceMarker, corners, queryImg):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    cv2.fillPoly(queryImg, corners.astype(int), (230, 230, 230))    
-    (textX, textY) = np.abs(corners[0][0] + corners[0][2]) / 2    
-    textsizeX, textsizeY = cv2.getTextSize(str(distanceMarker), font, 1, 3)[0]    
-    textX = (textX - textsizeX / 2).astype(int)
-    textY = (textY + textsizeY / 2).astype(int)    
-    cv2.putText(queryImg, str(distanceMarker), (textX, textY), font, 1, (0, 0, 0), 2)
-    return queryImg
-
-
-#def findRectangles(imgRect, idsR, cornersR):
-#    debug = 0
-#    # Grayscale image is requested for contour recognition
-#    imgRectGray = cv2.cvtColor(imgRect, cv2.COLOR_BGR2GRAY)
-#
-#    # Check if at least one marker has been found
-#    if idsR is None or len(idsR) == 0:
-#        # If no marker detected, exit
-#        print("No marker detected!")
-#        return None
-#
-#    # Print found arucos
-#    if debug:
-#        for i, corner in zip(idsR, cornersR):
-#            print('Detected aruco with ID: {}.'.format(i[0]))
-#
-#    #======== Find contours in image ========
-#        
-#    # The "findContours" function nedd a binary image, so need to threeshold before
-#    #ret, imgThresh = cv2.threshold(imgRectGray, 127, 255, 0)
-#    imgThresh = cv2.adaptiveThreshold(imgRectGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 2)
-#    contours, hierarchy = cv2.findContours(imgThresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-#
-#    # Identify rectangular contours
-#    rect_cnts = []
-#    areas = []
-#    for cnt in contours:
-#        peri = cv2.arcLength(cnt, True)
-#        approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
-#        #(x, y, w, h) = cv2.boundingRect(cnt)
-#        #ar = w / float(h)
-#        if len(approx) == 4: # shape filtering condition
-#            # Get the area of the rectangle, need to exclude rectangles with area less than the 
-#            # one of the smallest aruco
-#            area = cv2.contourArea(cnt)
-#
-#            # Exclude rectangles with pixel area, due to some threesholding error perhaps
-#            if area >= 5.0:
-#                areas.append(area)
-#                rect_cnts.append(cnt) # Shape is rectangle, add to the valid list
-#    # Now in rect_cnts[] we have only rectangular contours
-#
-#    #======== Discard the contours that do not contain any aruco (multiple markers can be present in the image)
-#
-#    # Make a copy to preserve the original image, draw functions are destructive
-#    imgRectDraw = np.copy(imgRect)
-#
-#    j = 0
-#    in_cnt = [] # dim (2,2) array of markers containing array of contours for each marker
-#    for aruco_n, corner_n in zip(idsR, cornersR): # for every aruco marker in image...
-#        cnt_father = [] # collect contours, for each marker
-#        corner_n = corner_n[0].astype(int) # adjust array dimensionality
-#        if debug:
-#            imgRectDraw = cv2.circle(imgRectDraw, (corner_n[0][0], corner_n[0][1]), 50, (0,0,255), 3)
-#        i=0
-#        for cnt in rect_cnts: # for every rectangular contour...
-#            dist = cv2.pointPolygonTest(cnt, (corner_n[0][0], corner_n[0][1]), True) # Check if top left corner of the aruco
-#            # dist is:
-#            # - dist<0 if point is outside contour
-#            # - dist=0 if point is in the contour itself
-#            # - dist>0 if point is inside
-#            # Note that the ==0 is not exactly zero, can be 0.5, so a threshold is needed
-#            # Check difference in area: must be 10% greater than the one of the aruco
-#            if (dist > 1.) and (areas[i] > cv2.contourArea(corner_n)*1.20): # if the aruco is inside the contour...
-#                cnt_father.append(cnt) # add the contour in list
-#                if debug:
-#                    print("Contour distance:", dist)
-#                    cv2.drawContours(imgRectDraw, [cnt], -1, (0,255,0), 2) # for debug draw the contour found
-#            i+=1
-#        if len(cnt_father) != 0:
-#            in_cnt.append(cnt_father) # check next aruco
-#        
-#    return in_cnt
-    
-
-def drawSingleAru(queryImg, corners, imgpts):
-    corner = tuple(corners.ravel())
-    queryImg = cv2.line(queryImg, corner, tuple(imgpts[0].ravel()), (255,0,0), 3)
-    queryImg = cv2.line(queryImg, corner, tuple(imgpts[1].ravel()), (0,255,0), 3)
-    queryImg = cv2.line(queryImg, corner, tuple(imgpts[2].ravel()), (0,0,255), 3)
-    return queryImg
-
-
-# def cut_markers_area(queryImg,corners,rotation_matrix):
-    
-#         rotatedImg = cv2.warpAffine(queryImg, rotation_matrix, _img_rot.shape[1::-1], flags=cv2.INTER_LINEAR)
-#         _extrema = cv2.perspectiveTransform(np.array([maxx,maxy,minx,miny]), _rot_mat)
-                
-#         img_mrk.append(_img_rot[_extrema[0][1]:_extrema[1][1], _extrema[0][0]:_extrema[1][0], :])
-        
-#         i+=1
-#     return img_mrk
-    
-
-########### GEOMETRIC FUNCTIONS ####################
  
 
 def singleAruRelPos(queryImg,corners,Id,markerSize_mm,camera_matrix,camera_dist_coefs, 
@@ -470,7 +339,3 @@ def singleAruRelPos(queryImg,corners,Id,markerSize_mm,camera_matrix,camera_dist_
 #    
 #    centerx,centery=np.abs(corners[0][0] + corners[0][2])/2
 """
-##################################
-#   bibliography
-#    https://docs.opencv2.org/4.2.0/d5/dae/tutorial_aruco_detection.html
-#    for the parameters list
