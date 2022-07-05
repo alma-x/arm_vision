55bbb7b60cf72f026ecf74dc04e53d798f665b23
gnoccoalpesto <74739268+gnoccoalpesto@users.noreply.github.com>
2022-07-05 21:24:31 +0000

    inquirer for found arucos, td3

--- scripts/inquirer_aruco.py
+++ scripts/inquirer_aruco.py
@@ -6,12 +6,10 @@ import os
 from geometry_msgs.msg import Pose
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
-
+from arm_vision.msg import FoundArucos
 from arm_control.srv import aruco_service,aruco_serviceResponse
 from arm_control.srv import cv_server,cv_serverResponse, cv_serverRequest
 from arm_control.msg import cv_to_bridge as bridge_msg
-
-pub = rospy.Publisher('aruco_bridge_opencv', bridge_msg, queue_size=1)
 bool_exit=False
 
 targetList=[[1,50],
@@ -45,38 +43,81 @@ markers_dict={'button_1':           1,
               'lid_storage':        14
               }
 
+findings_topic="/found_arucos"
+base_frame='base_link'
+
 aruco_success=False
 
 targetCounter=0
 targetListLen=len(targetList)
 remaining_targets=targetListLen
 
-# COMMENTATO
-global findNewTarget
-findNewTarget=1
-
-
-#def receiveTargetRequest():
-#    selectedTarget=read_somewhere(targetRequestTopic)
-#    (targetMarkId,targetMarkSize)=loadTargetData(selectedTarget)
-
-
-#def loadTargetData(requestString):
-#    return aruTargetDict[requestString]
-
+# global findNewTarget
+# findNewTarget=1
 
 # global targetMarkId,targetMarkSize
 
+found_markers=[]
 available_ids=[]
 available_corners=[]
 
-def at_least_one_marker_found():return True
+msgVector=[0,0,0]#np.zeros([1,3])
+msgRotMatrix=[[0,0,0,],[0,0,0],[0,0,0]]#np.zeros([3,3])
 
-def getArucoPose():
-    pose=Pose()
-    return pose
+tglWristLengthRecovery=1
+# recovered percentage
+recovLenRatio=1
 
-def callbackRaw(raw_img):
+tf_buffer=None
+tf_listener=None
+
+def nameOfMarkerReference(id):
+    global markers_dict
+    for marker_name, marker_id in markers_dict.items():
+        if marker_id == id:
+            return marker_name
+
+
+def getArucoPose(marker_id):
+    marker_frame=nameOfMarkerReference(marker_id)
+    global tf_buffer
+    while True:
+        try:
+            return tf_buffer.lookup_transform(base_frame,marker_frame,rospy.Time()).transform
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, \
+            tf2_ros.ExtrapolationException): continue
+
+def getFoundMarkers(findings_msg):
+    global found_markers
+    found_markers=findings_msg.findings
+
+
+def rotationMatrixFromQuaternion(Q):
+    #https://www.meccanismocomplesso.org/en/hamiltons-quaternions-and-3d-rotation-with-python/
+    # R00=1-2*Q[1]*Q[1]-2*Q[2]*Q[2]
+    # R01=2*(Q[0]*Q[1]-Q[3]*Q[2])
+    # R02=2*(Q[0]*Q[2]+Q[3]*Q[1])
+    # R10=2*(Q[0]*Q[1]+Q[3]*Q[2])
+    # R11=1-2*Q[0]*Q[0]-2*Q[2]*Q[2]
+    # R12=2*(Q[1]*Q[2]-Q[3]*Q[0])
+    # R20=2*(Q[0]*Q[2]-Q[3]*Q[1])
+    # R21=2*(Q[1]*Q[2]+Q[3]*Q[0])
+    # R22=1-2*Q[0]*Q[0]-2*Q[1]*Q[1]
+    #https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
+    R00=2*(Q.x*Q.x+Q.y*Q.y)-1
+    R01=2*(Q.y*Q.z-Q.x*Q.w)
+    R02=2*(Q.y*Q.w+Q.x*Q.z)
+    R10=2*(Q.y*Q.z+Q.x*Q.w)
+    R11=2*(Q.x*Q.x+Q.z*Q.z)-1
+    R12=2*(Q.z*Q.w-Q.x*Q.y)
+    R20=2*(Q.y*Q.w-Q.x*Q.z)
+    R21=2*(Q.z*Q.w+Q.x*Q.y)
+    R22=2*(Q.x*Q.x-2*Q.w*Q.w)-1
+    R=[[R00,R01,R02],[R10,R11,R12],[R20,R21,R22]]
+    return np.array(R)
+
+
+def inquireFoundMarkers(_):
     global aruco_success
     global msgVector
     global msgRotMatrix
@@ -90,35 +131,20 @@ def callbackRaw(raw_img):
     msg=bridge_msg()
     msg.aruco_found=14*[False]
 
-    # (targetMarkId,targetMarkSize)=tuple(targetList[targetCounter])
-        
-    if at_least_one_marker_found():
-        
+    if np.any(found_markers):
         aruco_success=False 
-
-        for mId in available_ids:
-
-            if len(msg.aruco_found)>int(mId):
-                msg.aruco_found[int(mId)]=True
-
-            if mId==targetList[targetCounter][0]:    
-                Pmatr=getArucoPose(mId)
+        for found_id in [*np.where(found_markers)][0]:
+            msg.aruco_found[int(found_id)]=True
+            if found_id+1==targetList[targetCounter][0]:
+                tf_=getArucoPose(found_id+1)      
+                msgVector=[tf_.translation.x,tf_.translation.y,tf_.translation.z]  
+                msgRotMatrix=rotationMatrixFromQuaternion(tf_.rotation)
                 
-                rotMatr,tVect=Pmatr[0:3,0:3],Pmatr[0:3,3]
-                msgRotMatrix=rotMatr
-                msgVector=tVect
-                
-                aruco_success=True
-
-                # remaining_targets=targetListLen-targetCounter-1
-                #if targetCounter<targetListLen-1:
-                #    targetCounter+=1
-                
+                aruco_success=True       
     else:
         aruco_success=False
 
     msg.success=aruco_success
-
     msg.id_aruco=targetCounter+1
 
     if msg.success:
@@ -127,39 +153,26 @@ def callbackRaw(raw_img):
         msg.y=0.001*msgVector[1]
         msg.z=0.001*msgVector[2]
         msg.vector=msgRotMatrix.flatten()
-        #print(msg.vector)
     
-    pub.publish(msg)
+    bridge_pub.publish(msg)
             
 
-msgVector=[0,0,0]#np.zeros([1,3])
-msgRotMatrix=[[0,0,0,],[0,0,0],[0,0,0]]#np.zeros([3,3])
-
-        
-tglWristLengthRecovery=1
-# recovered percentage
-recovLenRatio=1
-
 def callback_service(req):
-    global aruco_success,msgVector,msgRotMatrix,targetCounter,findNewTarget,remaining_targets,bool_exit
+    global aruco_success,\
+            msgVector,\
+            msgRotMatrix,\
+            targetCounter,\
+            findNewTarget,\
+            remaining_targets,\
+            bool_exit
     
     print('Service received')
 
     if req.message=="exit":
         bool_exit=True
-
-    #if req.next_aruco:
     if req.message=="select_next_aruco":
-        #if targetCounter<targetListLen-1:
-        #    targetCounter=targetCounter+1
-        #remaining_targets=targetListLen-targetCounter-1
         targetCounter=int(req.second_information)-1
 
-    #print('Arucopy:\nService received')
-    #print('Service received')
-    #print('Target number:'+str(targetCounter))
-    #print('Remaining_targets:'+str(remaining_targets))
-    #print('TargetListLen:'+str(targetListLen))
     return cv_serverResponse(
         success=aruco_success,
         moreTargets=remaining_targets,
@@ -174,9 +187,19 @@ def callback_service(req):
 #------------------------------------------------------
 
 def arucoInquirer():
+    findings_sub=rospy.Subscriber(findings_topic,FoundArucos,getFoundMarkers,queue_size = 1)
+    
+    global  tf_buffer,\
+            tf_listener
+    tf_buffer=tf2_ros.Buffer(cache_time=rospy.Duration(1))
+    tf_listener=tf2_ros.TransformListener(tf_buffer,queue_size=None)
 
-    rospy.Subscriber(,queue_size = 1)
-    rospy.Publisher('aruco_bridge_opencv', bridge_msg, queue_size=10)
+    global bridge_pub
+    bridge_pub = rospy.Publisher('aruco_bridge_opencv', bridge_msg, queue_size=1)
+    
+    INQUIRE_DURATION=rospy.Duration(nsecs=1E6)
+    inquire_timer=rospy.Timer(INQUIRE_DURATION,inquireFoundMarkers)
+
     rospy.Service('cv_server', cv_server, callback_service)
     
 ###########################################################
